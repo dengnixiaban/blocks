a2c60bfdf89db13082ddecd246d75033f702fbcb
yuanchangxin <729056559@qq.com>
2019-08-30 17:36:09 +0800

    update

---
+++ user-api/src/main/java/cn/blocks/userapi/model/UserDTO.java
@@ -0,0 +1,20 @@
+package cn.blocks.userapi.model;
+
+import lombok.Data;
+
+/**
+ * @description
+ *          用户dto
+ *
+ * @auther Somnus丶y
+ * @date 2019/8/30 17:05
+ */
+@Data
+public class UserDTO {
+
+    private String id;
+
+    private String name;
+
+
+}
