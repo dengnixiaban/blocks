23b7d0df470919065d03240e7c3a402cb51a02c9
yuanchangxin <729056559@qq.com>
2019-09-09 11:31:44 +0800

    user-service日志,feign调用修改

--- user-service/src/main/java/cn/blocks/userservice/controller/UserController.java
+++ user-service/src/main/java/cn/blocks/userservice/controller/UserController.java
@@ -6,6 +6,7 @@ import cn.blocks.userapi.service.mono.IUserService;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.serializer.SerializerFeature;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
@@ -25,7 +26,7 @@ public class UserController implements IUserService {
 
 
     @Override
-    public Mono<UserDTO> userInfo(UserDTO userDTO) {
+    public Mono<UserDTO> userInfo(@ModelAttribute UserDTO userDTO) {
         LogUtils.info(log,"访问user-info接口,入参%s",
                 JSON.toJSONString(userDTO, SerializerFeature.DisableCircularReferenceDetect));
         return Mono.just(Optional.ofNullable(userDTO).orElse(new UserDTO("1","1",new Date())));
