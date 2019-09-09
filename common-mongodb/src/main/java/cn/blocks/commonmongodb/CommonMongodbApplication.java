408129711c554fc6157d8bbe355c1f250963c358
yuanchangxin <729056559@qq.com>
2019-09-09 19:27:39 +0800

    update

---
+++ common-mongodb/src/main/java/cn/blocks/commonmongodb/CommonMongodbApplication.java
@@ -0,0 +1,13 @@
+package cn.blocks.commonmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonMongodbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommonMongodbApplication.class, args);
+    }
+
+}
---
+++ common-netty/src/main/java/cn/blocks/commonnetty/CommonNettyApplication.java
@@ -0,0 +1,13 @@
+package cn.blocks.commonnetty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonNettyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommonNettyApplication.class, args);
+    }
+
+}
