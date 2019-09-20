5d668671353340540e54ec810de1f62b05a87fd0
yuanchangxin <729056559@qq.com>
2019-09-20 16:08:10 +0800

    redis init

---
+++ common-redis/src/main/java/cn/blocks/commonredis/config/SingleRedisConfiguration.java
@@ -0,0 +1,17 @@
+package cn.blocks.commonredis.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @description
+ *          redis 单机配置
+ *
+ * @author Somnus丶y
+ * @date 2019/9/20 16:06
+ */
+@Configuration
+public class SingleRedisConfiguration {
+
+
+
+}
